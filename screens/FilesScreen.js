ce3e5175cc70a6c3f88f409c074c7e01347124c1
Isaac Miller <isaacbmiller@gmail.com>
2021-02-25 11:58:13 -0600

    Added working arrows

--- screens/FilesScreen.js
+++ screens/FilesScreen.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { View, Text, TouchableOpacity, Image, StyleSheet } from "react-native";
+import { Icon } from "react-native-elements";
 
 const photos = [require("../purpleheart.jpeg"), require("../test1.jpg"), require("../grandpaknife.jpeg")];
 
@@ -11,9 +12,26 @@ const FilesScreen = () => {
         setPhoto(photos[count]);
     },[count]);
     return (
-        <View>
-            <Image style={{width: 300, height: 300}} source={photo}></Image>
-            <TouchableOpacity onPress></TouchableOpacity>
+        <View style={{flexDirection: "row", flex: 1}}>
+            {count > 0 ? <TouchableOpacity onPress={() => {setCount(count-1)}}>
+                <Icon
+                    type="font-awesome-5"
+                    name="chevron-left"
+                    color="black"
+                    iconStyle={{ fontSize: 20 }}
+                    
+                />
+            </TouchableOpacity> : null}
+            <Image style={{width: "80%", height: "100%"}} source={photo}></Image>
+            {count < photos.length - 1 ? <TouchableOpacity onPress={() => {setCount(count+1)}}>
+                <Icon
+                    type="font-awesome-5"
+                    name="chevron-right"
+                    color="black"
+                    iconStyle={{ fontSize: 20 }}
+                    
+                />
+            </TouchableOpacity> : null}
         </View>
     )
 }

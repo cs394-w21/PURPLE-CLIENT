cbbbff1cba2166dd698e455d7fd539c637e2f9e4
Isaac Miller <isaacbmiller@gmail.com>
2021-03-03 11:21:23 -0600

    Started Styling create story screen

--- components/create-story/CreateStoryScreen.js
+++ components/create-story/CreateStoryScreen.js
@@ -12,37 +12,98 @@ import { Icon } from "react-native-elements";
 import GradientButton from "../GradientButton";
 
 const CreateStoryScreen = ({ route, navigation }) => {
+  const [name, setName] = useState("")
+  const [summary, setSummary] = useState("")
+
   return (
     <View>
-      <Text></Text>
-      <View>
-        <View></View>
+      <Text style={styles.title}>Create Your Story</Text>
+      {/* Progress bar */}
+      <View style={styles.barBackground}>
+        <View style={styles.bar}></View>
       </View>
       <View>
-        <Text>Tell me your special story in 3 easy steps...</Text>
+        <Text style={styles.text1}>Tell me your special story in 3 easy steps... </Text>
 
-        <Text>
-          TTell your story through text, upload any photos or videos, and record
+        <Text style={styles.text2}>
+          Tell your story through text, upload any photos or videos, and record
           your story through audio.
         </Text>
 
-        <Text>
-          TDon’t Worry. Each step is optional and can be edited later so you can
+        <Text style={styles.text3}>
+          Don't Worry. Each step is optional and can be edited later so you can
           tell your story your way.
         </Text>
       </View>
-      <View>
-        <Text>Story Name</Text>
-        <TextInput></TextInput>
+      <View >
+        <Text style={styles.textFieldTitle}>Story Name</Text>
+        <TextInput style={styles.textFieldContent}></TextInput>
       </View>
 
-      <View>
-        <Text>Short Summary</Text>
-        <TextInput></TextInput>
+      <View >
+        <Text style={styles.textFieldTitle}>Short Summary</Text>
+        <TextInput style={styles.textFieldContent}></TextInput>
       </View>
       <GradientButton onPress={() => console.log("yerr")} title={"Continue"} />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  text1: {
+    fontFamily: "Roboto",
+    fontStyle:  "normal",
+    fontWeight: "bold",
+    color: "#666666",
+    fontSize:   12,
+    lineHeight: 14,
+  },
+  text2: {
+    fontFamily: "Roboto",
+    fontStyle:  "normal",
+    fontWeight: "normal",
+    color: "#666666",
+    fontSize:   12,
+    lineHeight: 14,
+  },
+  text3: {
+    fontFamily: "Roboto",
+    fontStyle:  "italic",
+    fontWeight: "medium",
+    color: "#000000",
+    fontSize:   12,
+    lineHeight: 14,
+  },
+  textFieldTitle: {
+    fontFamily: "Roboto",
+    fontStyle:  "normal",
+    fontWeight: "normal",
+    color: "#666666",
+    fontSize:   16,
+    lineHeight: 20,
+  },
+  textFieldContent: {
+    fontFamily: "Roboto",
+    fontStyle:  "italic",
+    fontWeight: "Light",
+    color: "#666666",
+    fontSize:   16,
+    lineHeight: 20,
+  },
+  title: {
+    fontFamily: "Roboto",
+    fontStyle:  "normal",
+    fontWeight: "bold",
+    color: "#FB37FF",
+    fontSize:   24,
+    lineHeight: 20,
+  },
+});
 
 export default CreateStoryScreen;
